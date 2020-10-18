ca22abba546c855d4d3a123544005b7e7df43876
David Acero <acero.david.17@gmail.com>
2020-10-17 23:32:44 -0500

    Add Selenium Folder

--- seleniumTutorial/basicWeb/01_initialTest.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-class initialTest():
-    def test(self):
-        driver = webdriver.Chrome()
-        driver.get("https://futura.com.mx/")
-        
-cc = initialTest()
-cc.test()
